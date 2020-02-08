d149fd2c12e3b0ade62cd03a58ad166fbaa28bb9
Daniel <danielmutu96@hotmail.it>
2020-02-08 16:22:36 +0100

    home page

--- www/js/customIndex.js
+++ www/js/customIndex.js
@@ -1,3 +1,8 @@
 
-
-alert("hello");
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
